e4001dfdcefae651b2064c6a103b8c7ecbc30613
Todor Nikolov <Todor Nikolov>
2017-02-15 22:07:19 +0200

    Initial commit for really simple console project with the latest .NET Core and VSCode tools

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AppleStore;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Apple a = new Apple("red", "round", 12);
+            System.Console.WriteLine(a.CreateApple());
+        }
+    }
+}
---
+++ store.commons/common.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Name
+{
+    class FirstClass
+    {
+        public FirstClass(string[] args) {
+            
+        }
+        public int someRandomProperty { get; set; }
+    }
+
+    namespace SecondNameSpace
+    {
+        class SecondClass
+        {
+            public SecondClass()
+            {
+                
+            }
+
+            public int someRandomProperty { get; set; }
+        }
+    }
+}
